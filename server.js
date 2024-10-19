47c415ee94fb6a711ea1a4e02e0530ee7577449e
nepjl12 <elongoprince1995@gmail.com>
2024-10-19 06:50:53 +0100

    connexion front

--- server.js
+++ server.js
@@ -17,6 +17,18 @@ app.use(express.json());
 app.use(cors());
 main
 
+app.use((req, res, next) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  
+    next();
+  
+  });
+
 app.use('/auth', verifierToken);
 app.use('/user', userRoutes);
 app.use('/car', carRoutes);
