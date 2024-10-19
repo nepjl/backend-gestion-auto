d142002e533b62e87ef1d64a3e5a709bb8ec71e8
nepjl12 <elongoprince1995@gmail.com>
2024-10-19 03:26:14 +0100

    Ajout de la route Dashboard

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { userSeConnecter, userDeconnecter } from '../controllers/userController.js';
+import { userSeConnecter, getUserProfile, userDeconnecter } from '../controllers/userController.js';
+import { verifierToken } from '../middlewares/userAuth.js';
 
 const Route = express.Router();
 
 Route.post('/login', userSeConnecter);
 Route.get('/logout', userDeconnecter);
+Route.get('/dashboard', verifierToken, getUserProfile );
 
 
 
